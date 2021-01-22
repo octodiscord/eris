c6025d29383aa6e9a2084d198d0f650e46cf950d
Axis <54381371+ax1s01@users.noreply.github.com>
2021-01-22 05:27:52 -0500

    upd: Fixing error handling

--- index.d.ts
+++ index.d.ts
@@ -1871,7 +1871,7 @@ declare namespace Eris {
     editAFK(afk: boolean): void;
     editStatus(status?: Status, game?: ActivityPartial<BotActivityType>): void;
     editStatus(game?: ActivityPartial<BotActivityType>): void;
-    // @ts-expect-error: Method override
+    // @ts-ignore
     emit(event: string, ...args: any[]): void;
     getGuildMembers(guildID: string, timeout: number): void;
     hardReset(): void;
